4b5340fc258d8ae4790cdc8f99031f193f9c4899
Squamto <25984337+Squamto@users.noreply.github.com>
2023-10-28 19:49:37 +0200

    relative path

--- zig-imgui/imgui_backend.zig
+++ zig-imgui/imgui_backend.zig
@@ -1,4 +1,18 @@
+const std = @import("std");
+
+fn joinWithLocation(path: []const u8) []const u8 {
+    const allocator = std.heap.page_allocator;
+
+    const base = @src().file.dir;
+
+    const joined = std.fs.path.join(allocator, .{
+        base, path,
+    });
+    defer allocator.free(joined);
+    return joined;
+}
+
 pub const c = @cImport({
-    @cInclude("vendor/cimgui/imgui/imgui_impl_glfw.h");
-    @cInclude("vendor/cimgui/imgui/imgui_impl_vulkan.h");
+    @cInclude(joinWithLocation("vendor/cimgui/imgui/imgui.h"));
+    @cInclude(joinWithLocation("vendor/cimgui/imgui/imgui_impl_vulkan.h"));
 });
