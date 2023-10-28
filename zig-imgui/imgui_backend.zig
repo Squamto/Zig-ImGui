206cc2fd80999981d51174a36138ccfd20e66c5d
Squamto <25984337+Squamto@users.noreply.github.com>
2023-10-28 20:14:40 +0200

    .

--- zig-imgui/imgui_backend.zig
+++ zig-imgui/imgui_backend.zig
@@ -1,4 +1,14 @@
-pub const c = @cImport({
-    @cInclude("vendor/cimgui/imgui/imgui.h");
-    @cInclude("vendor/cimgui/imgui/imgui_impl_vulkan.h");
-});
+// pub const c = @cImport({
+//     @cInclude("vendor/cimgui/imgui/imgui_impl_vulkan.h");
+//     @cInclude("vendor/cimgui/imgui/imgui_impl_glfw.h");
+// });
+
+const std = @import("std");
+
+const glfw = @import("mach_glfw");
+
+pub extern fn ImGui_ImplGlfw_InitForVulkan(window: glfw.Window, install_callbacks: bool) bool;
+
+test "123" {
+    std.testing.refAllDecls(@This());
+}
